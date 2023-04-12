74e7e7fe181d1799a12d7d42ac7efe32c7528fed
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-04-12 12:30:48 +0200

    YDM-5_YDM-7 / clean code

--- src/main/java/pl/szczesniak/dominik/whattowatch/movies/domain/model/exceptions/MovieDoestNotBelongToUserException.java
+++
@@ -1,9 +0,0 @@
-package pl.szczesniak.dominik.whattowatch.movies.domain.model.exceptions;
-
-public class MovieDoestNotBelongToUserException extends RuntimeException {
-
-	public MovieDoestNotBelongToUserException(final String message) {
-		super(message);
-	}
-
-}
