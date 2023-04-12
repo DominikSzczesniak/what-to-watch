278a61fc97adbe95c51bd15db793078eb4d560ef
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-04-12 12:32:05 +0200

    YDM-5_YDM-7 / clean code

--- src/main/java/pl/szczesniak/dominik/whattowatch/users/domain/model/UserId.java
+++ src/main/java/pl/szczesniak/dominik/whattowatch/users/domain/model/UserId.java
@@ -1,21 +1,13 @@
 package pl.szczesniak.dominik.whattowatch.users.domain.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-@EqualsAndHashCode
-@ToString
-public class UserId {
+public record UserId(@Getter int value) {
 
-	@Getter
-	private final int value;
-
-	public UserId(final int value) {
+	public UserId {
 		checkArgument(value > 0, "UserId value must be higher than 0");
-		this.value = value;
 	}
 
 }
