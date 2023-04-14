11e8150c8fe9234f29cf3353a7a844963b8bef7d
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-04-14 10:05:21 +0200

    YDM-5_YDM-7 / movietitle check

--- src/main/java/pl/szczesniak/dominik/whattowatch/movies/domain/model/MovieTitle.java
+++ src/main/java/pl/szczesniak/dominik/whattowatch/movies/domain/model/MovieTitle.java
@@ -2,9 +2,15 @@ package pl.szczesniak.dominik.whattowatch.movies.domain.model;
 
 import lombok.Value;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 @Value
 public class MovieTitle {
 
 	String value;
 
+	public MovieTitle(final String value) {
+		checkArgument(value != null, "Movie title can't be null");
+		this.value = value;
+	}
 }
