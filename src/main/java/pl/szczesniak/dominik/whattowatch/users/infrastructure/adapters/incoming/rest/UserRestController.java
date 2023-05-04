051527aaa5df7ccb40fa0d812e192c963079b9bc
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-05-04 10:38:48 +0200

    spring_and_rest // optimized imports

--- src/main/java/pl/szczesniak/dominik/whattowatch/users/infrastructure/adapters/incoming/rest/UserRestController.java
+++ src/main/java/pl/szczesniak/dominik/whattowatch/users/infrastructure/adapters/incoming/rest/UserRestController.java
@@ -3,12 +3,10 @@ package pl.szczesniak.dominik.whattowatch.users.infrastructure.adapters.incoming
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
