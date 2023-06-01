d8ec6057b28ebf01bb5da9b39ae1583bb688c6bf
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-06-01 16:54:34 +0200

    YDM-23 // clean code

--- src/main/java/pl/szczesniak/dominik/whattowatch/users/domain/model/UserId.java
+++ src/main/java/pl/szczesniak/dominik/whattowatch/users/domain/model/UserId.java
@@ -1,8 +1,6 @@
 package pl.szczesniak.dominik.whattowatch.users.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 import lombok.Value;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -10,11 +8,10 @@ import static com.google.common.base.Preconditions.checkArgument;
 @Value
 public class UserId {
 
-	@Getter
 	int value;
 
 	@JsonCreator
-	public UserId(@JsonProperty("value") final int value) {
+	public UserId(final int value) {
 		this.value = value;
 		checkArgument(value > 0, "UserId value must be higher than 0");
 	}
