508e6554eee4bbb1c9cce1ff8dc269922b27300a
DominikSzczęśniak <dominikszczesniak128@gmail.com>
2023-12-19 16:32:56 +0100

    frontend // update

--- frontend/UserScript.js
+++ frontend/UserScript.js
@@ -23,19 +23,46 @@ function loginUser() {
         })
         .then(data => {
             document.getElementById('response').innerText = `Logged in successfully. User ID: ${data}`;
+            showLoggedInState(username);
         })
         .catch(error => {
             document.getElementById('response').innerText = error.message;
         });
 }
 
-function createUser() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+function showLoggedInState(username) {
+    document.getElementById('loginContainer').style.display = 'none';
+    document.getElementById('loggedInContainer').style.display = 'block';
+    document.getElementById('loggedInUser').innerText = `Logged in as: ${username}`;
+
+    const userInfoContainer = document.getElementById('loggedInContainer');
+    userInfoContainer.style.position = 'absolute';
+    userInfoContainer.style.top = '10px';
+    userInfoContainer.style.right = '10px';
+}
+
+function logoutUser() {
+    document.getElementById('loginContainer').style.display = 'block';
+    document.getElementById('loggedInContainer').style.display = 'none';
+}
+
+function toggleCreateAccountForm() {
+    document.getElementById('loginContainer').style.display = 'none';
+    document.getElementById('createAccountContainer').style.display = 'block';
+}
+
+function toggleLoginForm() {
+    document.getElementById('loginContainer').style.display = 'block';
+    document.getElementById('createAccountContainer').style.display = 'none';
+}
+
+function createAccount() {
+    const newUsername = document.getElementById('newUsername').value;
+    const newPassword = document.getElementById('newPassword').value;
 
     const userDto = {
-        username: username,
-        password: password
+        username: newUsername,
+        password: newPassword
     };
 
     fetch('http://localhost:8080/api/users', {
@@ -53,9 +80,19 @@ function createUser() {
             }
         })
         .then(data => {
-            document.getElementById('response').innerText = `User created successfully. User ID: ${data}`;
+            const createResponseElement = document.getElementById('createResponse');
+            createResponseElement.style.color = '#008000'; // Green color
+            createResponseElement.innerText = `User created successfully. User ID: ${data}`;
         })
+        // .then(data => {
+        //     document.getElementById('createResponse').innerText = `User created successfully. User ID: ${data}`;
+        // })
         .catch(error => {
-            document.getElementById('response').innerText = error.message;
+            const createResponseElement = document.getElementById('createResponse');
+            createResponseElement.style.color = '#ff0000'; // Red color
+            createResponseElement.innerText = error.message;
         });
+        // .catch(error => {
+        //     document.getElementById('createResponse').innerText = error.message;
+        // });
 }
